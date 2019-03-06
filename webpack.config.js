ff87c1b66bd41f85d7cd079ebd6b412d2b533c2b
Alessio Michelini <alessio@pond5.com>
2019-03-06 11:26:02 +0000

    Fixed issue introduced by previous changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
