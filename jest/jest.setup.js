41332beee4ca5da815ddde4da1ac879269ebb24c
Alessio Michelini <alessio@musicphotographer.eu>
2021-07-17 16:55:05 +0100

    Updated dependencies Changed enzyme adapter to work with React 17

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -1,5 +1,5 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import jsdom from 'jest-environment-jsdom';
 import renderer from 'react-test-renderer';
 // React 16 Enzyme adapter
