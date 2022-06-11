abe3b9520f304678d70c07eb7a098e0232b1ffbf
Alessio Michelini <darkmavis1980@gmail.com>
2022-06-11 14:47:08 +0100

    Added support for JSX

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,36 @@
 module.exports = {
-  "extends": [
-    "airbnb-base",
-    "plugin:react/recommended"
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended'
   ],
-  "plugins": [
-    "react"
+  plugins: [
+    'react'
   ],
-  "settings": {
-    "react": {
-      "version": "16"
+  settings: {
+    react: {
+      version: '18'
     }
   },
-  "rules": {
-    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
-    "max-len": [1, 200],
-    "no-console": 1,
-    "new-cap": 0,
-    "comma-dangle": 0,
-    "object-curly-spacing": 0,
-    "quotes": 0,
-    "indent": ["warn", 2],
-    "arrow-parens": ["error", "as-needed"],
-    "class-methods-use-this": 0
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'max-len': [1, 200],
+    'no-console': 1,
+    'new-cap': 0,
+    'comma-dangle': 0,
+    'object-curly-spacing': 0,
+    'import/no-unresolved': 0,
+    quotes: 0,
+    indent: ['warn', 2],
+    'arrow-parens': ['error', 'as-needed'],
+    'class-methods-use-this': 0,
+    'import/extensions': [0, 'never'],
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     }
   },
-  "globals": {
+  globals: {
     window: true,
     document: true,
     jQuery: false,
