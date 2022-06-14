0b7cf4a74b87f5f8572871c021b583b39a92ddc5
Alessio Michelini <darkmavis1980@gmail.com>
2022-06-14 20:38:47 +0100

    Added error boundary

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React, { Suspense } from 'react';
 import Names from './Names';
+import ErrorBoundary from '../ErrorBoundary';
+import Loading from '../Loading';
 
 const Home = () => (
   <div>
     <h1>Hello World</h1>
-    <Suspense fallback={<p>Loading...</p>}>
-      <Names />
-    </Suspense>
+    <ErrorBoundary fallback={<p>Could not load the data from the API.</p>}>
+      <Suspense fallback={<Loading />}>
+        <Names />
+      </Suspense>
+    </ErrorBoundary>
   </div>
 );
 
