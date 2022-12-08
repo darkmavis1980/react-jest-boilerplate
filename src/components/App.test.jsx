57c71ea741448eccddcdb6b8cd20f727de061dc8
Alessio Michelini <darkmavis1980@gmail.com>
2022-12-08 22:28:00 +0000

    Updated dependencies Fixed issue with window object and Jest

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import TestRenderer from 'react-test-renderer';
 import App from './App';
 
-const wrapper = TestRenderer.create(<App />);
+const wrapper = TestRenderer.create(<StrictMode><App /></StrictMode>);
 const instance = wrapper.root;
 
 describe('(Component) App', () => {
   it('should match the snapshot', () => {
-    const tree = renderer
-      .create(<App />)
+    const tree = TestRenderer
+      .create(<StrictMode><App /></StrictMode>)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
